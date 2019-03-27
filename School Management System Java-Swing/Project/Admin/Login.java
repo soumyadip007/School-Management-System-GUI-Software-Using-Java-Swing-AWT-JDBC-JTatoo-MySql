22887f919b690056cb59c40e0a4423d6ccbdd6b0
soumyadip007 <soumyadip.note@gmail.com>
2019-03-27 19:16:21 +0530

    Admin Login

--- School Management System Java-Swing/Project/Admin/Login.java
+++ School Management System Java-Swing/Project/Admin/Login.java
@@ -1,9 +1,141 @@
 package Admin;
 
-public class Login {
+import java.awt.Color;
 
+import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Toolkit;
+
+import javax.swing.border.EmptyBorder;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+
+public class Login extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JPasswordField passwordField;
+	private JButton btnNewButton;
+	private JButton btnNewButton_1;
+	private String user[]={"Soumyadip","Soumya"};
+	private String pass[]={"abcd","abcde"};
+	private JLabel lblNewLabel_1;
+	private JLabel label;
+	private JLabel label_1;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setTitle("School Management System");
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
 	public Login() {
-		// TODO Auto-generated constructor stub
+		//setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\Soumyadeep\\Desktop\\Book Hub\\0.jpg"));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(450, 190, 1014, 597);
+		setResizable(false);
+		
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Admin Login");
+		lblNewLabel.setForeground(new Color(255, 255, 102));
+		lblNewLabel.setFont(new Font("Times New Roman", Font.PLAIN, 46));
+		lblNewLabel.setBounds(364, 60, 273, 93);
+		contentPane.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setFont(new Font("Tahoma", Font.PLAIN, 32));
+		textField.setBounds(481, 170, 281, 68);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		passwordField.setFont(new Font("Tahoma", Font.PLAIN, 32));
+		passwordField.setBounds(481, 286, 281, 68);
+		contentPane.add(passwordField);
+		
+		JLabel lblUsername = new JLabel("Username");
+		lblUsername.setForeground(new Color(255, 255, 255));
+		lblUsername.setFont(new Font("Tahoma", Font.PLAIN, 31));
+		lblUsername.setBounds(250, 166, 193, 52);
+		contentPane.add(lblUsername);
+		
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setForeground(new Color(255, 255, 255));
+		lblPassword.setBackground(Color.CYAN);
+		lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 31));
+		lblPassword.setBounds(250, 286, 193, 52);
+		contentPane.add(lblPassword);
+		
+		btnNewButton = new JButton("Login");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 26));
+		btnNewButton.setBounds(545, 392, 162, 73);
+		btnNewButton.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				int i=0,j=0;
+				String ustr=textField.getText();
+				String pstr=passwordField.getText();
+				
+				
+				if( ustr.equals("1234") && pstr.equals("1234"))
+				{
+					dispose();
+				//	AdminHost ah=new AdminHost();
+				//	ah.setTitle("Book Hub");
+				//	ah.setVisible(true);
+					JOptionPane.showMessageDialog(btnNewButton, "You have successfully logged in");
+				}
+				else
+				{
+					JOptionPane.showMessageDialog(btnNewButton, "Wrong Username & Password");
+				}
+			}
+		});
+	
+		contentPane.add(btnNewButton);
+		
+		btnNewButton_1 = new JButton("User Login");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				//Userlogin us=new Userlogin();
+				//us.setTitle("Book Hub");
+				//us.setVisible(true);
+			}
+		});
+		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 26));
+		btnNewButton_1.setBounds(250, 387, 193, 83);
+		contentPane.add(btnNewButton_1);
+		
+		label_1 = new JLabel("");
+		//label_1.setIcon(new ImageIcon("C:\\Users\\Soumyadeep\\Desktop\\Book Hub\\0.jpg"));
+		label_1.setBounds(0, 0, 1008, 562);
+		contentPane.add(label_1);
+		
+	
+
 	}
 
 }
