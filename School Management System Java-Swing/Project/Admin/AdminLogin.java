c75a8b1184649a6e97f21e4cd1cf1d1eb62585b7
soumyadip007 <soumyadip.note@gmail.com>
2019-04-01 18:34:51 +0530

    Login page updated

--- School Management System Java-Swing/Project/Admin/AdminLogin.java
+++ School Management System Java-Swing/Project/Admin/AdminLogin.java
@@ -81,11 +81,11 @@ public class AdminLogin extends JFrame {
 		passwordField.setBounds(481, 286, 281, 68);
 		contentPane.add(passwordField);
 		
-		JLabel lblUsername = new JLabel("Username");
+		JLabel lblUsername = new JLabel("Faculty-ID");
 		lblUsername.setBackground(Color.BLACK);
 		lblUsername.setForeground(Color.BLACK);
 		lblUsername.setFont(new Font("Tahoma", Font.PLAIN, 31));
-		lblUsername.setBounds(250, 166, 193, 52);
+		lblUsername.setBounds(250, 178, 193, 52);
 		contentPane.add(lblUsername);
 		
 		JLabel lblPassword = new JLabel("Password");
